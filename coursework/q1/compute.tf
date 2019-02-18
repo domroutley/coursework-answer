37500608b619e3ac387c1bf6db9466f7359a237b
domroutley <dominic.routley@gmail.com>
2019-02-18 23:52:11 +0000

    changed names and moved to static ip

---
+++ coursework/q1/compute.tf
@@ -0,0 +1,35 @@
+resource "openstack_compute_instance_v2" "prod" {
+  image_name      = "Ubuntu Server 18.04.1"         # The name of the image that is to be used
+  flavor_name     = "m1.micro"                      # The flavor of server to use
+  name            = "Production Webserver"          # The unique name for this instance
+  key_pair        = "pheonix"                       # What ssh key pair to use
+  security_groups =                                 # What security group/s are associated with instance
+    ["${openstack_networking_secgroup_v2.web_sec_group.name}"]
+  network {                                         # Select what network/s to assign instance to
+    name = "${openstack_networking_network_v2.webservers_network.name}"
+  }
+}
+
+resource "openstack_compute_instance_v2" "dev" {
+  image_name      = "Ubuntu Server 18.04.1"
+  flavor_name     = "m1.micro"
+  name            = "Development Webserver"
+  key_pair        = "pheonix"
+  security_groups = [
+    "${openstack_networking_secgroup_v2.web_sec_group.name}",
+    "${openstack_networking_secgroup_v2.direct_access_sec_group.name}"
+  ]
+  network {
+    name = "${openstack_networking_network_v2.webservers_network.name}"
+  }
+}
+
+resource "openstack_compute_floatingip_associate_v2" "prod_ip_assoc" {
+  floating_ip = "10.72.96.170"
+  instance_id = "${openstack_compute_instance_v2.prod.id}"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "dev_ip_assoc" {
+  floating_ip = "10.72.96.124"
+  instance_id = "${openstack_compute_instance_v2.dev.id}"
+}
