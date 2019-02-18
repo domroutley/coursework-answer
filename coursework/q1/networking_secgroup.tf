d6a09d5d4573916e1f2b14a47befc3379375473e
domroutley <dominic.routley@gmail.com>
2019-02-18 23:52:45 +0000

    added security group rules

---
+++ coursework/q1/networking_secgroup.tf
@@ -0,0 +1,42 @@
+resource "openstack_networking_secgroup_v2" "web_sec_group" {
+  name        = "Web sec group"
+  description = "Webserving security group"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "web_sec_grp_rule_https" {
+  description       = "HTTPS"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 88
+  port_range_max    = 88
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.web_sec_group.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "web_sec_grp_rule_http" {
+  description       = "HTTP"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.web_sec_group.id}"
+}
+
+resource "openstack_networking_secgroup_v2" "direct_access_sec_group" {
+  name        = "Access sec group"
+  description = "Direct access to server security group"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "sec_grp_rule_ssh" {
+  description       = "SSH"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.direct_access_sec_group.id}"
+}
