515f00bfd06fc57586a09299cb4ffd2bd3c082b0
domroutley <dominic.routley@gmail.com>
2019-02-19 19:44:36 +0000

    rganising for cleanliness

--- coursework/q1/compute.tf
+++ coursework/q1/compute.tf
@@ -1,15 +1,20 @@
 resource "openstack_compute_instance_v2" "prod" {
-  image_name      = "Ubuntu Server 18.04.1"           # The name of the image that is to be used
-  flavor_name     = "m1.micro"                        # The flavor of server to use
-  name            = "Production Webserver"            # The unique name for this instance
-  key_pair        = "${var.internal_access_key_name}" # What ssh key pair to use
-  security_groups =                                   # What security group/s are associated with instance
+  image_name      = "Ubuntu Server 18.04.1"                                     # The name of the image that is to be used
+  flavor_name     = "m1.micro"                                                  # The flavor of server to use
+  name            = "Production Webserver"                                      # The unique name for this instance
+  key_pair        = "${var.internal_access_key_name}"                           # What ssh key pair to use
+  security_groups =                                                             # What security group/s are associated with instance
     ["${openstack_networking_secgroup_v2.web_sec_group.name}",
     "${openstack_networking_secgroup_v2.internal_access_sec_group.name}"
   ]
-  network {                                           # Select what network/s to assign instance to
+  network {                                                                     # Select what network/s to assign instance to
     name = "${openstack_networking_network_v2.network_1.name}"
   }
+}
+
+resource "openstack_compute_floatingip_associate_v2" "prod_ip_assoc" {
+  floating_ip = "10.72.96.170"
+  instance_id = "${openstack_compute_instance_v2.prod.id}"
 }
 
 resource "openstack_compute_instance_v2" "dev" {
@@ -24,11 +29,6 @@ resource "openstack_compute_instance_v2" "dev" {
   network {
     name = "${openstack_networking_network_v2.network_1.name}"
   }
-}
-
-resource "openstack_compute_floatingip_associate_v2" "prod_ip_assoc" {
-  floating_ip = "10.72.96.170"
-  instance_id = "${openstack_compute_instance_v2.prod.id}"
 }
 
 resource "openstack_compute_floatingip_associate_v2" "dev_ip_assoc" {
