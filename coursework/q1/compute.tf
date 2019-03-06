6357e7a483dc4839cd0e558b2de45211b3328854
domroutley <dominic.routley@gmail.com>
2019-03-06 15:15:25 +0000

    changed from Openstack stored keys to locally stored keys

--- coursework/q1/compute.tf
+++ coursework/q1/compute.tf
@@ -2,7 +2,7 @@ resource "openstack_compute_instance_v2" "prod" {
   image_name      = "Ubuntu Server 18.04.1"                                     # The name of the image that is to be used
   flavor_name     = "m1.micro"                                                  # The flavor of server to use
   name            = "Production Webserver"                                      # The unique name for this instance
-  key_pair        = "${var.prod_access_key_name}"                               # What ssh key pair to use
+  key_pair        = "${openstack_compute_keypair_v2.prod.name}"                      # What ssh key pair to use
   security_groups =                                                             # What security group/s are associated with instance
     ["${openstack_networking_secgroup_v2.web_sec_group.name}"
   ]
@@ -16,11 +16,16 @@ resource "openstack_compute_floatingip_associate_v2" "prod_ip_assoc" {
   instance_id = "${openstack_compute_instance_v2.prod.id}"
 }
 
+resource "openstack_compute_keypair_v2" "prod" {
+  name       = "${var.prod_access_key_name}"
+  public_key = "${var.prod_access_key}"
+}
+
 resource "openstack_compute_instance_v2" "dev" {
   image_name      = "Ubuntu Server 18.04.1"
   flavor_name     = "m1.micro"
   name            = "Development Webserver"
-  key_pair        = "${var.dev_access_key_name}"
+  key_pair        = "${openstack_compute_keypair_v2.dev.name}"
   security_groups = [
     "${openstack_networking_secgroup_v2.web_sec_group.name}"
   ]
@@ -32,4 +37,9 @@ resource "openstack_compute_instance_v2" "dev" {
 resource "openstack_compute_floatingip_associate_v2" "dev_ip_assoc" {
   floating_ip = "${openstack_networking_floatingip_v2.dev_floating_ip.address}"
   instance_id = "${openstack_compute_instance_v2.dev.id}"
+}
+
+resource "openstack_compute_keypair_v2" "dev" {
+  name       = "${var.dev_access_key_name}"
+  public_key = "${var.dev_access_key}"
 }
--- coursework/q1/vars.tf
+++ coursework/q1/vars.tf
@@ -19,6 +19,14 @@ variable "dev_access_key_name" {
   default = "dragon"
 }
 
+variable "dev_access_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClS6FHJDURSOxx5Kle4xQZ2mFXPE2Lvs5rFy2SFw1PJrLm4Ob+ZXw8xRgm4yOj60GU4psGaeADlZ7gsWPLa5yB2k8qjIttSSAfeLNGhU0l3MYrgjDCRZhVM45mzhudiU0R7dVlsTGT5fOGktiQCuJXEiE9680C4UbePuUnyOayFlRHw9bg/zttbglHLWpKEVgxSPmhszmYRtM0/sU9eCT3lk7QmMrErO6747v4KRfMLiqZ13wxt4NYmniDQ+bk7NSlcCOou2ZU7thV3zAd4WXLZYIjZdmu31PfUG4vmGSuwvT0WGjEZAAgXLc0T1GYrWY0lbFlhSgtPv8/J7ymnQiD dom@1Ap70P"
+}
+
 variable "prod_access_key_name" {
   default = "pheonix"
+}
+
+variable "prod_access_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAYmceMK5NQcwozc32s3l03efbleekifVBmkJoc+79Tm/j3iY8KOBsPpD3yblnURxK4aKPhsZsIwMp2soHLcitOSnPlFKfFFg+ELxDZTnyt25xPGpfBecXd4rPTEMS/1V0SOeYYWNgzqfzugsD9ORsfCRaUon81xNZC6rWQK0H9E0dwpQfU/woF2a8zqBZNoR9k2oPwZlZPfyaHFyQe4IP1jjWSIeqm7qe+KRRpBZ69+b10ngrlMQE2lslSQTz1HjtXRcfId/QhCItlZIKoLLS0n4di1oqfg5YbOOXDB/5CJOj7zNrZdeVPXFPI+WvcdwDzA1O34yU+S7tZteQOAgh dom@1Ap70P"
 }
